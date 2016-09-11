4c86a06f2f0885d2188b1f3153689963d9d51ad2
sh1zbr0 <stevenprc2new@gmail.com>
2016-09-11 23:30:52 +1200

    Implemented -O3 optimization argument in GCC compilation

--- cythonize.sh
+++ cythonize.sh
@@ -5,5 +5,5 @@ rm ./serpdm
 echo "Cythonizing ser-pdm.py..."
 cython --embed -o serpdm.c ./serpdm.py
 echo "DONE. Now attempting to compile serpdm.c"
-gcc -Os -I /usr/include/python2.7 -o serpdm serpdm.c -lpython2.7 -lpthread -lm -lutil -ldl
+gcc -O3 -I /usr/include/python2.7 -o serpdm serpdm.c -lpython2.7 -lpthread -lm -lutil -ldl
 echo "all done! Now try executing ./serpdm . It SHOULD be faster!"
